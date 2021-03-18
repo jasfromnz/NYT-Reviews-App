7358b4c000c8e926d8fe665a5166bf48370f9ab1
Jasmine Guy <jasfromnz@gmail.com>
2021-03-17 23:03:02 -0500

    adds basic functionality to script.js

--- js/script.js
+++ js/script.js
@@ -5,18 +5,50 @@ const API_KEY = "&api-key=9KYhx592sZnxcKpcAq5NzytfNGc9jztp";
 
 // state variables
 
-let $reviews;
+let userInput;
+let reviews;
+
+// cached element references
+
+let $reviews = $('.reviews');
+let $submit = $('#submit');
+let $input = $('#input');
 
 // event listeners
 
-/*submit button*/
+console.log($submit);
+$submit.on('click', handleClick);
 
 // functions
 
 function getData() {
-    
+    $.ajax(BASE_URL+userInput+API_KEY)
+        .then(function(data){
+            reviews = data.results;
+            render();
+        }, function(error){
+            alert(error);
+        });
 }
 
 function render() {
+    const html = reviews.map(function(review){
+        if (review.summary === '') {
+            review.summary = 'Unavailable';
+        };
+        return `
+            <article class="card">
+                <h2>${review.book_title} by ${review.book_author}</h2>
+                <h3>review by: ${review.byline}, ${review.publication_dt}</h3>
+                <p id="summary">Summary: ${review.summary}</p>
+                <p id="url">URL: ${review.url}</p>
+            </article>
+        `;
+    });
+    $reviews.append(html);
+}
 
+function handleClick(){
+    userInput = $input.val();
+    getData();
 }
