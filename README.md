d5d02d4013742bbca2716c2ccff06b7dcb9fd16a
Jasmine Guy <jasfromnz@gmail.com>
2021-03-17 20:37:53 -0500

    wip: adds basic setup for HTML, README, and JS

---
+++ README.md
@@ -0,0 +1,18 @@
+# NYT Book Reviews
+
+A web app that searches for and displays New York Times book reviews by book title.
+
+## Technologies
+- HTML5
+- CSS3
+- JavaScript
+- jQuery
+- New York Times Books API
+
+## Screenshots
+![wireframe](#)
+
+## Getting Started
+[Click here](#) to get started
+
+## Future Enhancements
---
+++ js/script.js
@@ -0,0 +1,22 @@
+// constant variables
+
+const BASE_URL = "https://api.nytimes.com/svc/books/v3/reviews.json?title=";
+const API_KEY = "&api-key=9KYhx592sZnxcKpcAq5NzytfNGc9jztp";
+
+// state variables
+
+let $reviews;
+
+// event listeners
+
+/*submit button*/
+
+// functions
+
+function getData() {
+    
+}
+
+function render() {
+
+}
